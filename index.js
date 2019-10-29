91ffbb15a4c390e1735b9a5c9af966203f3c0b46
Tiago Boer Casagrande <49966908+tboerc@users.noreply.github.com>
2019-10-29 07:52:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Dimensions } from "react-native";
 const { width } = Dimensions.get("window").width;
-const rem = width >= 600 ? width / 1440 : width / 1080;
+const pr = width >= 600 ? width / 1440 : width / 1080;
 
-export default rem;
+export default pr;
