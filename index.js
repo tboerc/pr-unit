5b7bf31c3b9c473f5d18def23cfbcdf48936715e
tboerc <tiago@hybriun.com>
2019-10-29 08:25:02 -0300

    Fixed package functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Dimensions } from "react-native";
-const { width } = Dimensions.get("window").width;
+const { width } = Dimensions.get("window");
 const pr = width >= 600 ? width / 1440 : width / 1080;
 
 export default pr;
