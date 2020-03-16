092e6b2f4d24cb86822398832ae28761195b9f16
Tiago Boer Casagrande <tiago@hybriun.com.br>
2020-03-16 08:10:12 -0300

    Melhoria na performance

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { Dimensions } from "react-native";
 const { width } = Dimensions.get("window");
 const pr = width >= 600 ? width / 1440 : width / 1080;
 
-export default pr;
+export default pr.toFixed(3);
