63ad961f6331dbb499d5dba390cf360f3341c0b6
tboerc <tiago.boer@hotmail.com>
2020-05-31 11:44:55 -0300

    Fixed wrong types and blacklisting values

---
+++ src/blacklist.ts
@@ -0,0 +1,22 @@
+const blacklist: Array<string> = [
+  'aspectRatio',
+  'bottom',
+  'elevation',
+  'end',
+  'left',
+  'flex',
+  'flexBasis',
+  'flexGrow',
+  'flexShrink',
+  'lineHeight',
+  'letterSpacing',
+  'maxLength',
+  'opacity',
+  'right',
+  'shadowOpacity',
+  'start',
+  'top',
+  'zIndex',
+];
+
+export default blacklist;
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
-import {Dimensions, ViewStyle, StyleSheet as ReactStyleSheet} from 'react-native';
+// Imports
+import {Dimensions, ViewStyle, ImageStyle, TextStyle, StyleSheet as ReactStyleSheet} from 'react-native';
+import blacklist from './blacklist';
 
+// Types and Interfaces
+interface Config {
+  unit: number;
+  blacklist: Array<string>;
+}
+
+type NamedStyles<T> = {[P in keyof T]: ViewStyle | TextStyle | ImageStyle};
+
+// Base pr calc
 const {width} = Dimensions.get('window');
 const pr = Math.round((width >= 600 ? width / 1440 : width / 1080) * 1e3) / 1e3;
 
+// Default config object
+const _config: Config = {unit: pr, blacklist};
+
+// Function to check key in blacklist
+const checkBlackList = (blacklist: Array<string>, key: string) => !blacklist.some((value) => key == value);
+
 class StyleSheet {
+  static compose = ReactStyleSheet.compose;
   static absoluteFill = ReactStyleSheet.absoluteFill;
   static hairlineWidth = ReactStyleSheet.hairlineWidth;
 
-  static create(style: {[key: string]: ViewStyle}, unit: number = pr): object {
-    const predstyle: {[key: string]: any} = {};
-
-    Object.entries(style).forEach(([name, object]) => {
-      Object.entries(object).forEach(([key, value]) => {
-        if (typeof value === 'number') predstyle[name][key] = value * unit;
-        else predstyle[name][key] = value;
+  static create<T extends NamedStyles<T> | NamedStyles<any>>(style: T | NamedStyles<T>, config: Config = _config) {
+    Object.entries(style).forEach(([key, value]) => {
+      Object.entries(value).forEach(([subKey, subValue]) => {
+        if (typeof value === 'number' && checkBlackList(config.blacklist, subKey))
+          style[key][subKey] = subValue * config.unit;
+        else style[key][subKey] = subValue;
       });
     });
-    return ReactStyleSheet.create(predstyle);
+    return ReactStyleSheet.create(style);
   }
 }
 
